4443ee57fecddad87c12b3eb1c4b0c8037cb06e5
Richard B <86970639+chillman159@users.noreply.github.com>
2021-12-13 15:46:41 +0100

    DB persistante

---
+++ test/api/src/main/java/com/openclassrooms/api/Country.java
@@ -0,0 +1,23 @@
+package com.openclassrooms.api;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Table(name="country")
+@Entity
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    //...
+}
